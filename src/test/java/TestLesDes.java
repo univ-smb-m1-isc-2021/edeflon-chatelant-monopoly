d4cd02ffff9b1d09ddedd03633e1390b20ce71a8
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-01-17 16:46:12 +0100

    fix LesDesTest

--- src/test/java/TestLesDes.java
+++ src/test/java/TestLesDes.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 public class TestLesDes {
-    public TestLesDes(){
-        test();
-    }
-    
     @Test
     public void test() {
         LesDes d = new LesDes();
