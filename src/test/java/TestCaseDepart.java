2d5f16e7ff192e36c4166c52676ca216bc5d92c8
Elodie D <elodie.deflon@gmail.com>
2022-01-17 17:27:49 +0100

    Create TestCaseDepart.java

---
+++ src/test/java/TestCaseDepart.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+public class TestCaseDepart {
+    @Test
+    public void test() {
+        Case depart = new CaseDepart();
+        Case c1 = new CaseChance();
+        Case c2 = new CaisseCommu();
+
+        depart.setSuivant(c1);
+        c1.setSuivant(c2);
+        c2.setSuivant(depart);
+
+        Personnage p = new Personnage("Willy", c2);
+
+        // Le joueur gagne 200 en passant par la case départ
+        int soldeInitial = p.solde;
+        p.setPosition(p.position.avancer(p, 2));
+        int soldeFinal = p.solde;
+
+        assert((soldeFinal - soldeInitial) == 200);
+
+        // Le joueur gagne 400 en tombant sur la case départ
+        soldeInitial = p.solde;
+        p.setPosition(p.position.avancer(p, 2));
+        p.setPosition(p.position.avancer(p, 1));
+        soldeFinal = p.solde;
+
+        assert((soldeFinal - soldeInitial) == 400);
+    }
+}
